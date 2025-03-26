26466f370656cf9fc0ba834f184f42e734216cf8
Dasha <dashakonopelko@gmail.com>
2025-03-26 17:02:31 +0200

    thread

---
+++ 03_thread_example_app/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _03_thread_example_app;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
